b18aba6e0052558f0f347fc3796e0ffa4b59b794
Monroe Gollaher <mgollaher92@gmail.com>
2021-03-08 16:40:07 -0700

    basic operation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,36 +1,112 @@
-import React from "react"
+import React, { useState } from "react"
 import Button from '../Button'
 import "./App.css"
 
 const App = () => {
+  const [value, setValue] = useState("0")
+  const [memory, setMemory] = useState(null)
+  const [operator, setOperator] = useState(null)
+ 
+
+  const handleButtonPress = content => () => {
+    const num = parseFloat(value)
+
+    if(content === 'AC') {
+      setValue("0")
+      setMemory(null)
+      setOperator(null)
+      return
+    }
+
+    if(content === "±") {
+      setValue((num * -1).toString())
+      return
+    }
+
+    if(content === "%") {
+      setValue((num / 100).toString())
+      setMemory(null)
+      setOperator(null)
+      return
+    }
+
+    if(content === '+') {
+      setMemory(parseFloat(value))
+      setValue("0")
+      setOperator("+")
+      return
+    }
+
+    if(content === '−') {
+      setMemory(parseFloat(value))
+      setValue("0")
+      setOperator("-")
+      return
+    }
+
+    if(content === '×') {
+      setMemory(parseFloat(value))
+      setValue("0")
+      setOperator("×")
+      return
+    }
+
+    if(content === '÷') {
+      setMemory(parseFloat(value))
+      setValue("0")
+      setOperator("÷")
+      return
+    }
+
+    if(content === '=') {
+      if(!operator) return 
+
+      if(operator === '+') {
+        setValue((memory + parseFloat(value)).toString())
+      } else if(operator === '-') {
+        setValue((memory - parseFloat(value)).toString())
+      } else if(operator === '×') {
+        setValue((memory * parseFloat(value)).toString())
+      } else if(operator === '÷') {
+        setValue((memory / parseFloat(value)).toString())
+      }
+
+      setMemory(null)
+      setOperator(null)
+      return
+    }
+
+    setValue((parseFloat(num + content)).toString())
+  }
+
   return (
     <div className="App">
       <div className="top">
         4:43
       </div>
       <div className="display">
-        0
+        { value }
       </div>
       <div className="buttons">
-        <Button content="AC" type="function" />
-        <Button content="±" type="function" />
-        <Button content="%" type="function" />
-        <Button content="÷" type="operator" />
-        <Button content="7" />
-        <Button content="8" />
-        <Button content="9" />
-        <Button content="×" type="operator" />
-        <Button content="4" />
-        <Button content="5" />
-        <Button content="6" />
-        <Button content="−" type="operator" />
-        <Button content="1" />
-        <Button content="2" />
-        <Button content="3" />
-        <Button content="+" type="operator" />
-        <Button content="0" />
-        <Button content="." />
-        <Button content="=" type="operator" />
+        <Button onButtonClick={ handleButtonPress } content="±" type="function" />
+        <Button onButtonClick={ handleButtonPress } content="%" type="function" />
+        <Button onButtonClick={ handleButtonPress } content="AC" type="function" />
+        <Button onButtonClick={ handleButtonPress } content="÷" type="operator" />
+        <Button onButtonClick={ handleButtonPress } content="7" />
+        <Button onButtonClick={ handleButtonPress } content="8" />
+        <Button onButtonClick={ handleButtonPress } content="9" />
+        <Button onButtonClick={ handleButtonPress } content="×" type="operator" />
+        <Button onButtonClick={ handleButtonPress } content="4" />
+        <Button onButtonClick={ handleButtonPress } content="5" />
+        <Button onButtonClick={ handleButtonPress } content="6" />
+        <Button onButtonClick={ handleButtonPress } content="−" type="operator" />
+        <Button onButtonClick={ handleButtonPress } content="1" />
+        <Button onButtonClick={ handleButtonPress } content="2" />
+        <Button onButtonClick={ handleButtonPress } content="3" />
+        <Button onButtonClick={ handleButtonPress } content="+" type="operator" />
+        <Button onButtonClick={ handleButtonPress } content="0" />
+        <Button onButtonClick={ handleButtonPress } content="." />
+        <Button onButtonClick={ handleButtonPress } content="=" type="operator" />
       </div>
       <div className="bottom">
         -
