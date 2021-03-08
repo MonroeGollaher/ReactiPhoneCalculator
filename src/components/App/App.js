b557ebd4266792dd77cf50e91013dde5c599f674
Monroe Gollaher <mgollaher92@gmail.com>
2021-03-08 15:53:48 -0700

    buttons & button styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,7 +12,25 @@ const App = () => {
         0
       </div>
       <div className="buttons">
-        <Button />
+        <Button content="AC" type="function" />
+        <Button content="±" type="function" />
+        <Button content="%" type="function" />
+        <Button content="÷" type="operator" />
+        <Button content="7" />
+        <Button content="8" />
+        <Button content="9" />
+        <Button content="×" type="operator" />
+        <Button content="4" />
+        <Button content="5" />
+        <Button content="6" />
+        <Button content="−" type="operator" />
+        <Button content="1" />
+        <Button content="2" />
+        <Button content="3" />
+        <Button content="+" type="operator" />
+        <Button content="0" />
+        <Button content="." />
+        <Button content="=" type="operator" />
       </div>
       <div className="bottom">
         -
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 import "./Button.css"
 
-const Button = (props) => {
+const Button = ({ content, type }) => {
   return (
-    <div className="Button">
-      AC
+    <div className={ `Button ${ content === '0' ? 'zero' : '' } ${ type || ''} `}>
+      { content }
     </div>
   )
 }
